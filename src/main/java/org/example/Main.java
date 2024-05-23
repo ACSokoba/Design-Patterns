d64e8a570d160566d16629aecc6adc84a965bf75
ACSokoba <44870671+ACSokoba@users.noreply.github.com>
2024-05-23 22:44:52 +0200

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,21 @@
 package org.example;
 
+import org.example.singleton.Singleton;
 import org.example.strategy.Article;
 import org.example.strategy.CreditCardPaymentStrategy;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("-----Testing strategy-----");
         Article a = new Article();
         a.makePayment(new CreditCardPaymentStrategy());
+
+        System.out.println("-----Testing singleton-----");
+        Singleton singleInstance = Singleton.getInstance();
+        singleInstance.setName("Joe");
+        System.out.println("name in singleton " + singleInstance.getName());
+
     }
 }
---
+++ src/main/java/org/example/singleton/Singleton.java
@@ -0,0 +1,25 @@
+package org.example.singleton;
+
+public class Singleton {
+    private String name;
+
+    private static Singleton instance;
+
+    private Singleton() {
+
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null)
+            return new Singleton();
+        return instance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
